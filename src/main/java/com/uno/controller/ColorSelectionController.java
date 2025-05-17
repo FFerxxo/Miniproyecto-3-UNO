5c9101d00a41e72da9f29add8867f2f630cceb79
Andrés F. Barbosa <andres.barbosa@correounivalle.edu.co>
2025-05-17 15:10:14 -0500

    Inicia diseño de la interfaz grafica

--- src/main/java/com/uno/controller/ColorSelectionController.java
+++ src/main/java/com/uno/controller/ColorSelectionController.java
@@ -1,1 +1,78 @@
+package com.uno.controller;
+
+
+
+import com.uno.model.enums.CardColor;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+/**
+ * Controlador para la vista de selección de color.
+ */
+public class ColorSelectionController {
+
+    @FXML
+    private Rectangle redColorRect;
+
+    @FXML
+    private Rectangle blueColorRect;
+
+    @FXML
+    private Rectangle greenColorRect;
+
+    @FXML
+    private Rectangle yellowColorRect;
+
+    private Stage dialogStage;
+    private CardColor selectedColor;
+
+    /**
+     * Establece el escenario para este diálogo.
+     *
+     * @param dialogStage escenario del diálogo
+     */
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    /**
+     * Maneja el evento de selección de color.
+     *
+     * @param event evento de clic del ratón
+     */
+    @FXML
+    private void handleColorSelection(MouseEvent event) {
+        // Obtener el rectángulo que fue clicado
+        Rectangle clickedRect = (Rectangle) event.getSource();
+
+        // Determinar qué color se seleccionó
+        if (clickedRect == redColorRect) {
+            selectedColor = CardColor.RED;
+            System.out.println("Color seleccionado: Rojo");
+        } else if (clickedRect == blueColorRect) {
+            selectedColor = CardColor.BLUE;
+            System.out.println("Color seleccionado: Azul");
+        } else if (clickedRect == greenColorRect) {
+            selectedColor = CardColor.GREEN;
+            System.out.println("Color seleccionado: Verde");
+        } else if (clickedRect == yellowColorRect) {
+            selectedColor = CardColor.YELLOW;
+            System.out.println("Color seleccionado: Amarillo");
+        }
+
+        // Cerrar la ventana de diálogo
+        dialogStage.close();
+    }
+
+    /**
+     * Obtiene el color seleccionado.
+     *
+     * @return el color seleccionado
+     */
+    public CardColor getSelectedColor() {
+        return selectedColor;
+    }
+}
 
--- src/main/java/com/uno/view/MainApp.java
+++ src/main/java/com/uno/view/MainApp.java
@@ -1,8 +1,48 @@
 package com.uno.view;
 
 
+import com.uno.controller.GameController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Clase principal que inicia la aplicación JavaFX.
  */
 public class MainApp extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Cargar el archivo FXML de la vista del juego
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/uno/view/GameView.fxml"));
+        Parent root = loader.load();
+
+        // Obtener el controlador y configurar la ventana principal
+        GameController controller = loader.getController();
+        controller.setPrimaryStage(primaryStage);
+
+        // Configurar la escena
+        Scene scene = new Scene(root,1024,768);
+        scene.getStylesheets().add(getClass().getResource("/com/uno/view/css/styles.css").toExternalForm());
+
+        // Configurar el escenario
+        primaryStage.setTitle("UNO - Miniprojecto #3");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(true);
+        primaryStage.show();
+
+        // Iniciar el juego
+        controller.initGame();
+    }
+
+    /**
+     * Punto de entrada principal de la aplicación.
+     *
+     * @param args argumentos de línea de comandos
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
